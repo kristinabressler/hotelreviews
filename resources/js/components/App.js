f55250d8b384399a76b4b1c8665999ea49a1d184
kikibres <kristinabressler@yahoo.com>
2020-02-07 11:41:14 -0500

    editing App

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -45,14 +45,11 @@ class App extends Component {
             <div key={review.id} className="media">
                 <div className="media-body">
                     <p>{review.name}</p>
-                    <span className="text-muted">
+                    <div className="text-muted">
                             <br />
-                            by {review.user.name} |{" "}
-                            {review.updated_at
-                                .split(" ")
-                                .slice(1)
-                                .join(" ")}
-                        </span>
+                            by {review.user.name}
+                            {review.updated_at}
+                    </div>
                 </div>
             </div>
         ));

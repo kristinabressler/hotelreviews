4ffd37437a290a89c379720ac3dcf16a69783c58
kikibres <kristinabressler@yahoo.com>
2020-02-09 01:46:40 -0500

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
        
-        <!-- <link href="/css/app.css" rel="stylesheet" type="text/css"> -->
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>

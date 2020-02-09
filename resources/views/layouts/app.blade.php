d15538bf215bdc6072f5d92f799e27f98d928bd4
kikibres <kristinabressler@yahoo.com>
2020-02-09 02:29:11 -0500

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand navbar_logo" href="{{ url('/') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
                     <img src="/images/work2liveLogo.jpg" alt="" />
                 </a>

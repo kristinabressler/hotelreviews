abee38daeb453f227809714398de98eb31ec395e
kikibres <kristinabressler@yahoo.com>
2020-02-10 01:51:19 -0500

    css

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .styles('resources/css/style.css', 'public/css/all.css');
+   .styles('public/css/style.css', 'public/css/all.css');

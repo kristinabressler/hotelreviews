4f14613e4b5f0a9473c9b21435e8fdfaedd74f7c
kikibres <kristinabressler@yahoo.com>
2020-02-09 21:14:43 -0500

    editing style css

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .styles('public/css/style.css');
+   .sass('resources/sass/app.scss', 'public/css');
+   .styles('resources/css/style.css', 'public/css');
